656c2b2f1e24cc9db89ae668495cf8e71d4f1727
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2023-02-08 09:03:01 +0000

    rollback

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // base: '/',
   plugins: [react()],
-  build: {
-    target: 'es2020',
-  },
+  // build: {
+  //   target: 'es2020',
+  // },
 })
