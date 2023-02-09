50b45d32f75165bb93c487b0950e90911db8a771
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2023-02-09 04:01:38 +0000

    minor

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { Engine, Syncer, App } from '@dreamcatcher-tech/webdos'
 import { apps } from '@dreamcatcher-tech/interblock'
 
